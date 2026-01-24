ed62a5603dda15872838eca675afc3b783ff8467
Elaine Soares <elaine.fabiola@gmail.com>
2026-01-24 07:17:56 -0300

    importando e usando arquivo de CSS Modules com espoco de componete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
   console.log('Oi');
   return (
     <>
-      <h1>Olá Mundo!</h1>
+      <Heading> Olá Mundo</Heading>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste earum ut
         aperiam nobis hic, beatae itaque repellat dolore rerum est magnam at
