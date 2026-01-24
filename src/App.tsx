1d86591f8166d703ac50a8878912cd0501dd86ca
Elaine Soares <elaine.fabiola@gmail.com>
2026-01-24 11:45:39 -0300

    variavel e Rset de CSS do Chonos

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import './styles/global.css';
 import { Heading } from './components/Heading';
 
 export function App() {
-  console.log('Oi');
   return (
     <>
-      <Heading> Olá Mundo</Heading>
+      <Heading attr={123}>Olá Mundo</Heading>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste earum ut
         aperiam nobis hic, beatae itaque repellat dolore rerum est magnam at
