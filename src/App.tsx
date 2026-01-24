bd45bdf4d8ebefea864ff12652e574b77cae8d6f
Elaine Soares <elaine.fabiola@gmail.com>
2026-01-24 13:17:17 -0300

    Finalizacao do componente heading e tipagem TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading attr={123}>Olá Mundo</Heading>
+      <Heading>
+        Olá Mundo
+        <button>
+          {' '}
+          <TimerIcon />{' '}
+        </button>
+      </Heading>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste earum ut
         aperiam nobis hic, beatae itaque repellat dolore rerum est magnam at
