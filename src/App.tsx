15938d9ad9f56578d5837fe610d984fd25fc57c7
Elaine Soares <elaine.fabiola@gmail.com>
2026-01-29 21:31:13 -0300

    Criando o componente de logo com ícone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
+import { Logo } from './components/Logo';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo
-        <button>
-          {' '}
-          <TimerIcon />{' '}
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste earum ut
-        aperiam nobis hic, beatae itaque repellat dolore rerum est magnam at
-        porro consectetur officia ullam! Quos quia itaque maiores.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
